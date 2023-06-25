d2c6957e3727cc504c2ee9dd591b2e9241aa93e0
Rizal Pratama <zalllrizalll012@gmail.com>
2023-06-25 16:10:22 +0700

    Perbaikan UI

--- admin/index.php
+++ admin/index.php
@@ -46,6 +46,15 @@ $dsn_out = mysqli_query($conn, "SELECT COUNT(id) AS jumlah FROM log_akses_staf  
             </div>
           </div>
         </div>
+        <div class="col-xl-3 col-md-6">
+          <div class="card bg-dark text-white mb-4">
+            <div class="card-body"><b>Charts</b></div>
+            <div class="card-footer d-flex align-items-center justify-content-between">
+              <a class="small text-white stretched-link" href="charts.php">Show Details</a>
+              <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="row">
       <div class="col-xl-6">
