223cb197a582f98b4775fe2b81082ede8a5a0305
Rizal Pratama <zalllrizalll012@gmail.com>
2023-06-25 15:46:00 +0700

    Perbaikan UI

--- admin/header.php
+++ admin/header.php
@@ -65,7 +65,7 @@ if(!empty($_GET['status'])){
 <body class="sb-nav-fixed">
   <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="../landing/index.html">Bengkel Koding</a>
+    <a class="navbar-brand ps-3" href="../landing/index.html"><b>Bengkel Koding</b></a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
